f44b09cca5812c1fedb90e7c9774f60d0bd6843e
Kevin Paul <kevin@Rigel.localdomain>
2019-11-13 10:57:59 -0700

    Added web interface, DataStream module, and a lot of JavaScript

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,32 @@
 module ApplicationHelper
+    def decompressTelemetryString(datastream)
+        dataHeaders = [
+            :millis, 
+            :mission_elapsed, 
+            :bat_voltage, 
+            :time_since_last_sat_fix, 
+            :gps_satellites, 
+            :latitude, 
+            :longitude, 
+            :altitude, 
+            :heading, 
+            :downrange_dist, 
+            :downrange_bearing, 
+            :avg_ascent_rate, 
+            :last_rssi, 
+            :last_snr
+        ]
+
+        values = datastream.stream.remove("$").split(",")
+
+        decomp = Hash.new(values.length)
+        
+        for i in 0...dataHeaders.length
+            decomp[dataHeaders[i]] = values[i]
+        end
+
+        decomp[:db_time] = datastream.created_at.to_i
+
+        return decomp
+    end
 end
