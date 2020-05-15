c78416ba47ffce4f5e634fee552c68e44804c20c
Kevin Paul <krpaul2004@gmail.com>
2020-05-14 19:25:38 -0600

    Fixed migrations

--- db/migrate/20200514234228_create_images.rb
+++ db/migrate/20200514234228_create_images.rb
@@ -2,7 +2,8 @@ class CreateImages < ActiveRecord::Migration[6.0]
   def change
     create_table :images do |t|
       t.text :base64
-      
+      t.references :flight, null: false, foreign_key: true
+
       t.timestamps
     end
   end
